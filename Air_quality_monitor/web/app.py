a306caeb1f50d0c79f00fd7e69fb6b13f2076674
IrmA-ErroR <sveta.cat.kot@gmail.com>
2025-04-28 15:40:48 +0300

    Ready

--- Air_quality_monitor/web/app.py
+++ Air_quality_monitor/web/app.py
@@ -36,5 +36,9 @@ def index():
                          pm10=pm10,
                          records=data)
 
+@app.route('/health')
+def health():
+    return 'OK', 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
